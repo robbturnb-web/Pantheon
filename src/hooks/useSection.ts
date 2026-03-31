3e695d5a7137f1f791e1a15fcf09ff305bebdda5
robbturnb-web <robbturnb@gmail.com>
2026-03-31 02:29:52 -0400

    Revert "Add Pantheon Observatory: Full-stack esoteric research platform"

--- src/hooks/useSection.ts
+++
@@ -1,7 +0,0 @@
-import { useLocation } from 'react-router-dom';
-
-export function useSection(): string {
-  const { pathname } = useLocation();
-  if (pathname === '/') return 'home';
-  return pathname.replace('/', '');
-}
